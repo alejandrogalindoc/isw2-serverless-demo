b8f154fd6072940dfabc13e583119b2bd1480b02
alejandrogalindoc <alejandrogalindoc@gmail.com>
2026-01-30 17:26:33 -0600

    Update saludo.js

--- api/saludo.js
+++ api/saludo.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    mensaje: "`Hola ${nombre} 👋`,
+    mensaje: `Hola ${nombre} 👋`,
     curso: "Ingeniería de Software II",
     timestamp: new Date().toISOString()
   });
