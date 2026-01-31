4487a039fabe52adb1b9fb3850bc1c97a2e2c02c
alejandrogalindoc <alejandrogalindoc@gmail.com>
2026-01-30 20:50:10 -0600

    Update saludo.js

--- api/saludo.js
+++ api/saludo.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
-  const nombre = req.query.nombre || "anónimo";
-
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+    mensaje: "Hola ISW2",
+    curso: "Ingeniería de Software II",
+    timestamp: new Date().toISOString()
   });
 }
